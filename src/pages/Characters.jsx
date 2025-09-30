be7b054bc5e75f73c72c0792b75989f43c9f36b9
Imrane ALI LAFKIH <imr07ane@gmail.com>
2025-09-30 17:42:22 +0100

    refactor: update PostCardHelper and Characters components for smoother layout and flow

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -35,6 +35,7 @@ export default function Characters(){
     return(
 <>
         <CharacterSection data={characters[0]} />
+        <div className="h-[70vh]" />
          <CharacterSection data={characters[1]} switching={true} />
 </>
 
